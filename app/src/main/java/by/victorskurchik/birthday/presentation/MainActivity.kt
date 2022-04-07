39f61993af6596fe47e3179afa53175481caffe4
victorskurchik <victorskurchik@gmail.com>
2022-04-07 17:24:46 +0300

    add package for MainActivity.kt

--- app/src/main/java/by/victorskurchik/birthday/presentation/MainActivity.kt
+++ app/src/main/java/by/victorskurchik/birthday/presentation/MainActivity.kt
@@ -1,10 +1,12 @@
-package by.victorskurchik.birthday
+package by.victorskurchik.birthday.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import by.victorskurchik.birthday.screens.splash.SplashScreen
+import by.victorskurchik.birthday.presentation.screens.Navigation
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
