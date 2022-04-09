b6e1ef0d22175b92e7e9688dd78e886758f5081f
victorskurchik <victorskurchik@gmail.com>
2022-04-09 12:52:53 +0300

    add setAlarm todo

--- app/src/main/java/by/victorskurchik/birthday/presentation/screens/add/AddBirthdayViewModel.kt
+++ app/src/main/java/by/victorskurchik/birthday/presentation/screens/add/AddBirthdayViewModel.kt
@@ -1,21 +1,37 @@
 package by.victorskurchik.birthday.presentation.screens.add
 
+import android.app.AlarmManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import by.victorskurchik.birthday.domain.BirthdayRepository
 import by.victorskurchik.birthday.domain.entities.Birthday
+import by.victorskurchik.birthday.toDate
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
+
 
 @HiltViewModel
 class AddBirthdayViewModel @Inject constructor(
-    private val birthdayRepository: BirthdayRepository
+    private val birthdayRepository: BirthdayRepository,
+    private val alarmManager: AlarmManager
 ) : ViewModel() {
 
     fun addBirthday(birthday: Birthday) {
         viewModelScope.launch {
             birthdayRepository.addOrUpdateBirthday(birthday)
+            setAlarm(birthday.date)
         }
+    }
+
+    private fun setAlarm(date: String) {
+        val mDate = date.toDate()
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 19)
+        calendar.set(Calendar.MINUTE, 37)
+        calendar.set(Calendar.DAY_OF_MONTH, mDate.day)
+        calendar.set(Calendar.MONTH, mDate.month)
+        TODO()
     }
 }
